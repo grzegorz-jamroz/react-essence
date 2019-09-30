0a88aeaf95c07960f742d14a4ed5954e311c4eaa
Grzegorz <grzegorz.jamroz@esatto.se>
2019-09-30 14:29:15 +0200

    added new dependencies added new babel preset: metro-react-native-babel-preset

--- webpack.config.js
+++ webpack.config.js
@@ -2,55 +2,56 @@ var Encore = require('@symfony/webpack-encore');
 
 Encore
 // directory where compiled assets will be stored
-    .setOutputPath('public/build/')
-    // public path used by the web server to access the output path
-    .setPublicPath('/build')
-    // only needed for CDN's or sub-directory deploy
-    //.setManifestKeyPrefix('build/')
+  .setOutputPath('public/build/')
+  // public path used by the web server to access the output path
+  .setPublicPath('/build')
+  // only needed for CDN's or sub-directory deploy
+  //.setManifestKeyPrefix('build/')
 
-    /*
-     * ENTRY CONFIG
-     *
-     * Add 1 entry for each "page" of your app
-     * (including one that's included on every page - e.g. "app")
-     *
-     * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
-     */
-    .addEntry('app', './assets/js/app.js')
-    //.addEntry('page1', './assets/js/page1.js')
-    //.addEntry('page2', './assets/js/page2.js')
+  /*
+   * ENTRY CONFIG
+   *
+   * Add 1 entry for each "page" of your app
+   * (including one that's included on every page - e.g. "app")
+   *
+   * Each entry will result in one JavaScript file (e.g. app.js)
+   * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
+   */
+  .addEntry('app', './assets/js/App.js')
+  //.addEntry('page1', './assets/js/page1.js')
+  //.addEntry('page2', './assets/js/page2.js')
 
-    // will require an extra script tag for runtime.js
-    // but, you probably want this, unless you're building a single-page app
-    .enableSingleRuntimeChunk()
+  // will require an extra script tag for runtime.js
+  // but, you probably want this, unless you're building a single-page app
+  .enableSingleRuntimeChunk()
 
-    .cleanupOutputBeforeBuild()
-    .enableSourceMaps(!Encore.isProduction())
-    // enables hashed filenames (e.g. app.abc123.css)
-    .enableVersioning(Encore.isProduction())
+  .cleanupOutputBeforeBuild()
+  .enableSourceMaps(!Encore.isProduction())
+  // enables hashed filenames (e.g. app.abc123.css)
+  .enableVersioning(Encore.isProduction())
 
-// uncomment if you use TypeScript
-//.enableTypeScriptLoader()
+  // uncomment if you use TypeScript
+  //.enableTypeScriptLoader()
 
-// uncomment if you use Sass/SCSS files
-    .enableSassLoader()
+  // uncomment if you use Sass/SCSS files
+  .enableSassLoader()
 
-// uncomment if you're having problems with a jQuery plugin
-//.autoProvidejQuery()
+  // uncomment if you're having problems with a jQuery plugin
+  //.autoProvidejQuery()
 
-    // react
-    .enableReactPreset()
+  // react
+  .enableReactPreset()
 
-    // babel plugins
-    .configureBabel(function(babelConfig) {
-      babelConfig.plugins.push("@babel/plugin-proposal-class-properties");
-    })
+  // babel plugins
+  .configureBabel(function(babelConfig) {
+    babelConfig.plugins.push("@babel/plugin-proposal-class-properties");
+    babelConfig.presets.push("module:metro-react-native-babel-preset");
+  })
 
-    .addRule({
-      test: /\.VariablesExport.scss$/,
-      loaders: ["style-loader", "sass-loader"]
-    })
+  .addRule({
+    test: /\.VariablesExport.scss$/,
+    loaders: ["style-loader", "sass-loader"]
+  })
 ;
 
 module.exports = Encore.getWebpackConfig();
