8275a7774d869e63488608c6252e6acda042d79f
Grzegorz <grzegorz.jamroz@esatto.se>
2019-10-01 14:45:23 +0200

    added ProductSlider description

--- assets/js/components/ProductSlide/ProductSlide.js
+++ assets/js/components/ProductSlide/ProductSlide.js
@@ -9,7 +9,7 @@ const ProductSlide = ({ product }) => {
   return (
     <li className="glide__slide">
       <div className="productSlide">
-        <div className="productSlide__content">
+        <div className="productSlide__image">
           <img
             className="productSlide__img"
             src={require("../../../img/product/" + product.images[0])}
@@ -22,6 +22,23 @@ const ProductSlide = ({ product }) => {
           />
           <div className="product-favourite">
             <a href="#" className="favme fa fa-heart" />
+          </div>
+        </div>
+        <div className="productSlide__description">
+          <span>{product.label}</span>
+          <a href="#">
+            <h6>{product.name}</h6>
+          </a>
+          <p className="product-price">
+            <span className="old-price">
+              {product.reducedPrice}
+            </span>
+            {product.unitPrice}
+          </p>
+          <div className="hover-content">
+            <div className="add-to-cart-btn">
+              <a href="#" className="btn essence-btn">Add to Cart</a>
+            </div>
           </div>
         </div>
       </div>
