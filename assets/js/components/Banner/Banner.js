25f8c4a1aa60ff7cb78352995b4ff13852cadf25
Grzegorz <grzegorz.jamroz@esatto.se>
2019-10-01 09:40:59 +0200

    added some styles for banner and banner image

--- assets/js/components/Banner/Banner.js
+++ assets/js/components/Banner/Banner.js
@@ -8,8 +8,8 @@ const Banner = () => (
       <div className="banner__row row h-100">
         <div className="col-12">
           <div className="banner__content">
-            <h6>asoss</h6>
-            <h2>New Collection</h2>
+            <h6 className="banner__h6">asoss</h6>
+            <h2 className="banner__h2">New Collection</h2>
             <a href="#" className="btn essence-btn">view collection</a>
           </div>
         </div>
