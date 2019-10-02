6c401617d1cf9cfcc988b90f03954b2fbce01cdd
Grzegorz <grzegorz.jamroz@esatto.se>
2019-10-02 07:25:17 +0200

    recognizing and loading file versions inside build folder

--- public/index.php
+++ public/index.php
@@ -1,3 +1,12 @@
 <?php
 
-require_once __DIR__ . '/../index.html';
+$appJS = glob("build/app*.js");
+$runtimeJS = glob("build/runtime*.js");
+$appCSS = glob("build/app*.css");
+
+$html = file_get_contents('../index.html');
+$html = preg_replace('#build/app\.css#', $appCSS[0], $html);
+$html = preg_replace('#build/runtime\.js#', $runtimeJS[0], $html);
+$html = preg_replace('#build/app\.js#', $appJS[0], $html);
+
+echo $html;
