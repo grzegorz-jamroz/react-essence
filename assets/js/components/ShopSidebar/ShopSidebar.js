8e5bb69b0aa073f25d16609ef958e3e232b65328
Grzegorz <grzegorz.jamroz@esatto.se>
2019-10-08 14:56:08 +0200

    added ShopColors component

--- assets/js/components/ShopSidebar/ShopSidebar.js
+++ assets/js/components/ShopSidebar/ShopSidebar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ShopCategories from "../ShopCategories";
+import ShopColors from "../ShopColors";
 import ShopBrands from "../ShopBrands";
 
 const ShopSidebar = () => {
@@ -43,23 +44,7 @@ const ShopSidebar = () => {
         </div>
       </div>
 
-      <div className="widget color mb-50">
-        <p className="widget-title2 mb-30">Color</p>
-        <div className="widget-desc">
-          <ul className="d-flex">
-            <li><a href="#" className="color1" /></li>
-            <li><a href="#" className="color2" /></li>
-            <li><a href="#" className="color3" /></li>
-            <li><a href="#" className="color4" /></li>
-            <li><a href="#" className="color5" /></li>
-            <li><a href="#" className="color6" /></li>
-            <li><a href="#" className="color7" /></li>
-            <li><a href="#" className="color8" /></li>
-            <li><a href="#" className="color9" /></li>
-            <li><a href="#" className="color10" /></li>
-          </ul>
-        </div>
-      </div>
+      <ShopColors />
 
       <ShopBrands />
     </div>
