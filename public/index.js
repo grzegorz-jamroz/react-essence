eaaff7aa89a335677f77a66b8b4e22fffee5802e
Grzegorz <grzegorz.jamroz@esatto.se>
2019-10-10 10:58:05 +0200

    added route path for /shop

--- public/index.js
+++ public/index.js
@@ -3,11 +3,16 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
-router.get('/', function(req,res){
+router.get('/', function(req, res){
+  res.sendFile(path.join(__dirname + '/../index.html'));
+});
+
+router.get('/shop', function(req, res){
   res.sendFile(path.join(__dirname + '/../index.html'));
 });
 
 app.use('/', router);
-app.use('/build', express.static(path.join(__dirname, 'build')))
+app.use('/build', express.static(path.join(__dirname, 'build')));
+
 app.listen(3000);
 console.log(`listening on port 3000`);
